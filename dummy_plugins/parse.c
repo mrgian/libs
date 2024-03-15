4fdb3fcb00ee0f7a45dfe6b6965431989d6c5a2e
Gianmatteo Palmieri <mail@gian.im>
2024-03-15 11:35:47 +0100

    add dummy plugin
    Signed-off-by: Gianmatteo Palmieri <mail@gian.im>

---
+++ dummy_plugins/parse.c
@@ -0,0 +1,189 @@
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <sstream>
+#include <iostream>
+#include <unistd.h>
+
+#include "../libs/userspace/plugin/plugin_api.h"
+
+
+static constexpr uint16_t PPME_SYSCALL_OPEN_E = 2;
+static constexpr uint16_t PPME_SYSCALL_OPEN_X = 3;
+
+struct plugin_state
+{
+    std::string lasterr;
+    ss_plugin_table_t* thread_table;
+    ss_plugin_table_t* subtable;
+
+
+
+    //ss_plugin_table_field_t* table_field_comm;
+    ss_plugin_table_field_t* table_field_fdtable;
+    ss_plugin_table_field_t* table_field_fdtable_name;
+
+
+
+    ss_plugin_table_reader_vtable_ext* table_reader;
+
+    ss_plugin_state_data data;
+
+    ss_plugin_owner_t* owner;
+    ss_plugin_log_fn_t log;
+    bool once = false;
+    uint64_t count = 1000;
+};
+
+inline bool evt_type_is_open(uint16_t type)
+{
+    return type == PPME_SYSCALL_OPEN_E
+        || type == PPME_SYSCALL_OPEN_X
+    ;
+}
+
+extern "C" {
+
+const char* plugin_get_required_api_version()
+{
+    return PLUGIN_API_VERSION_STR;
+}
+
+const char* plugin_get_version()
+{
+    return "0.1.0";
+}
+
+const char* plugin_get_name()
+{
+    return "dummy";
+}
+
+const char* plugin_get_description()
+{
+    return "some desc";
+}
+
+const char* plugin_get_contact()
+{
+    return "some contact";
+}
+
+const char* plugin_get_parse_event_sources()
+{
+    return "[\"syscall\"]";
+}
+
+uint16_t* plugin_get_parse_event_types(uint32_t* num_types, ss_plugin_t* s)
+{
+    static uint16_t types[] = {
+        PPME_SYSCALL_OPEN_E,
+        PPME_SYSCALL_OPEN_X,
+    };
+    *num_types = sizeof(types) / sizeof(uint16_t);
+    return &types[0];
+}
+
+ss_plugin_t* plugin_init(const ss_plugin_init_input* in, ss_plugin_rc* rc)
+{
+    *rc = SS_PLUGIN_SUCCESS;
+    plugin_state *ret = new plugin_state();
+
+    //save logger and owner in the state
+    ret->log = in->log_fn;
+    ret->owner = in->owner;
+
+    ret->log(ret->owner, NULL, "initializing plugin...", SS_PLUGIN_LOG_SEV_INFO);
+
+    // get accessor for thread table
+    ret->thread_table = in->tables->get_table(in->owner, "threads", ss_plugin_state_type::SS_PLUGIN_ST_INT64);
+
+    ret->table_field_fdtable = in->tables->fields_ext->get_table_field(ret->thread_table, "fdtable", ss_plugin_state_type::SS_PLUGIN_ST_RAWPTR);
+    //ret->table_field_comm = in->tables->fields_ext->get_table_field(ret->thread_table, "comm", ss_plugin_state_type::SS_PLUGIN_ST_STRING);
+
+    ret->subtable = in->tables->fields_ext->get_subtable(ret->thread_table, ret->table_field_fdtable);
+
+    std::printf("Thread table addr: %p\n", ret->thread_table);
+    std::printf("Sub table addr: %p\n", ret->subtable);
+
+    ret->table_field_fdtable_name = in->tables->fields_ext->get_table_field(ret->subtable, "name", ss_plugin_state_type::SS_PLUGIN_ST_STRING);
+
+
+
+    if (!ret->thread_table)
+    {
+        *rc = SS_PLUGIN_FAILURE;
+        auto err = in->get_owner_last_error(in->owner);
+        ret->lasterr = err ? err : "can't access thread table";
+        return ret;
+    }
+
+    return ret;
+}
+
+void plugin_destroy(ss_plugin_t* s)
+{
+    plugin_state *ps = (plugin_state *) s;
+    ps->log(ps->owner, NULL, "destroying plugin...", SS_PLUGIN_LOG_SEV_INFO);
+
+    delete ((plugin_state *) s);
+}
+
+const char* plugin_get_last_error(ss_plugin_t* s)
+{
+    return ((plugin_state *) s)->lasterr.c_str();
+}
+
+ss_plugin_bool entry_iterator(ss_plugin_table_iterator_state_t* s, ss_plugin_table_entry_t* e)
+{
+    plugin_state *ps = (plugin_state *) s;
+
+    if(ps->count == 0)
+    {
+        return false;
+    }
+
+    ps->table_reader->read_entry_field(ps->thread_table, e, ps->table_field_fdtable, &ps->data);
+
+    std::printf("FD table addr for %p :\n", ps->data.rawptr);
+
+    ss_plugin_state_data key;
+    key.s64 = 0;
+
+    auto fdinfo = ps->table_reader->get_table_entry(ps->data.rawptr, &key);
+
+    ps->table_reader->read_entry_field(ps->data.rawptr, fdinfo, ps->table_field_fdtable_name, &ps->data);
+
+    std::printf("name: %s\n", ps->data.str);
+
+    ps->count--;
+    return true;
+}
+
+ss_plugin_rc plugin_parse_event(ss_plugin_t *s, const ss_plugin_event_input *ev, const ss_plugin_event_parse_input* in)
+{
+    plugin_state *ps = (plugin_state *) s;
+
+    if(ps->once)
+    {
+        return SS_PLUGIN_SUCCESS;
+    }
+
+    //const char* thread_table_name = in->table_reader_ext->get_table_name(ps->thread_table);
+    //uint64_t thread_table_size = in->table_reader_ext->get_table_size(ps->thread_table);
+
+
+    //const char* subtable_name = in->table_reader_ext->get_table_name(ps->subtable);
+    //uint64_t subtable_size = in->table_reader_ext->get_table_size(ps->subtable);
+
+    //std::printf("Thr table name %s\n", thread_table_name);
+    //std::printf("Sub table name %s\n", subtable_name);    
+
+    ps->table_reader = in->table_reader_ext;
+    in->table_reader_ext->iterate_entries(ps->thread_table, &entry_iterator, ps);
+
+    ps->once = true;
+    return SS_PLUGIN_SUCCESS;
+}
+
+}
